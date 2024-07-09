8ae5105968b3c43441fcc280d0f1d986b5ce1b18
Trynax <kandoso111@gmail.com>
2024-07-09 18:29:09 +0100

    handle add button

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,13 +1,28 @@
 import TodoList from "./components/TodoList"
 import TodoInput from "./components/TodoInput"
+import { useState } from "react"
 
 
 function App() {
 
+
+  const [todos, setTodos] = useState([
+  
+      "Go to the gym",
+      "Eat more fruits",
+      "Pickup kids from school"
+    ])
+
+    function handleAddTodos(todo) {
+      setTodos((currentTodos)=> [...currentTodos,todo])
+    }
+
   return (
    <>
-    <TodoInput/>
-    <TodoList/>
+    <TodoInput handleAddTodos={handleAddTodos} todos={todos}/>
+    <TodoList todos={todos
+
+    }/>
    </>
   )
 }
--- todo-list/src/components/TodoList.jsx
+++ todo-list/src/components/TodoList.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import TodoCard from './TodoCard'
 
-export default function TodoList() {
-
-  let todos = [
-    "Go to the gym",
-    "Eat more fruits",
-    "Pickup kids from school"
-  ] 
+export default function TodoList(props) {
+  const {todos}= props
   return (
     <ul className='main'>
       {todos.map((todo,todoIndex)=>{
