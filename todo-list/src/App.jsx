de7fafa2376c00a3e5d7839edffff46259fdfaab
Trynax <kandoso111@gmail.com>
2024-07-09 19:16:59 +0100

    final touch

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,6 +1,6 @@
 import TodoList from "./components/TodoList"
 import TodoInput from "./components/TodoInput"
-import { useState } from "react"
+import { useState,useEffect } from "react"
 
 
 function App() {
@@ -8,21 +8,45 @@ function App() {
 
   const [todos, setTodos] = useState([
     ])
+  const [todoValue, setTodoValue]= useState('')
+
+  function persistData(newList){
+    localStorage.setItem('todos',JSON.stringify({todos:newList}))
+  }
 
     function handleAddTodos(todo) {
       setTodos((currentTodos)=> [...currentTodos,todo])
+      persistData(todos)
     }
 
     function handleDeleteTodo(index){
       setTodos((currentTodos)=> currentTodos.filter((todo,todoIndex)=>{
         return todoIndex != index 
       }))
+      persistData(todos)
     }
 
+    function handleEditTodo(index){
+      const valueTobeEdited = todos[index]
+      setTodoValue(valueTobeEdited)
+    }
+
+    useEffect(()=>{
+      if(!localStorage){
+        return 
+      }
+      let localTodos = localStorage.getItem('todos')
+
+      if(!localTodos){
+        return
+      }
+localTodos= JSON.parse(localTodos).todos
+setTodos(localTodos) 
+    },[])
   return (
    <>
-    <TodoInput handleAddTodos={handleAddTodos} todos={todos}/>
-    <TodoList handleDeleteTodo={handleDeleteTodo} todos={todos
+    <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} todos={todos}/>
+    <TodoList todoValue={todoValue} setTodoValue={setTodoValue} handleEditTodo={handleEditTodo} handleDeleteTodo={handleDeleteTodo} todos={todos
 
     }/>
    </>
